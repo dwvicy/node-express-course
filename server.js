b647591eb9242ace10b0ad4d556101faa0385595
dwivi <vaishdw2207@gmail.com>
2020-05-24 16:18:05 +0530

    added first GET route

--- server.js
+++ server.js
@@ -4,6 +4,20 @@ const express = require('express');
 // Creates an express constructor instance called 'app'
 const app = express();
 
+const mockUserData  = [
+    {name: 'Ver'},
+    {name: 'Len '}
+]
+
+// this app.get method is used to get the route to the user data
+app.get('/users', function(req, res){
+    res.json({
+        success: true ,
+        message: 'successfully got users. Noice!',
+        users: mockUserData
+    })
+})
+
 // app.listen method will start up the server locally 
 // with the port which is provided by us as the first arg in this case 8000
 app.listen(8000,function(){
