87ce63e2d99a9b71bc66d26b68c2a3c336f1d48a
dwivi <vaishdw2207@gmail.com>
2020-05-24 16:24:45 +0530

    add dynamic GET route

--- server.js
+++ server.js
@@ -10,6 +10,9 @@ const mockUserData  = [
 ]
 
 // this app.get method is used to get the route to the user data
+// You just made your first endpoint! This function will respond to a GET request at http://localhost:8000/users with a JSON file, which includes our mockData under the key 'users'. Let's test it out!
+
+
 app.get('/users', function(req, res){
     res.json({
         success: true ,
@@ -17,7 +20,16 @@ app.get('/users', function(req, res){
         users: mockUserData
     })
 })
-
+// In Express, words with a colon in front of them in the url are treated as variables. 
+// You can access the value of each variable through req.params, like this:
+app.get('/users/:id',function(req,res){
+	console.log(req.params.id)
+	res.json({
+		success: true,
+		message: 'got one user',
+		user: req.params.id
+	})
+})
 // app.listen method will start up the server locally 
 // with the port which is provided by us as the first arg in this case 8000
 app.listen(8000,function(){
